0b8224fc52f6fdb7cdc57c0b9a858a4a9bb6a8cd
rruenroeng <rruenroeng@gmail.com>
2017-09-04 11:03:35 -0500

    Adding User Fields to ScoreForm. Not complete.

--- src/components/ScoreForm.js
+++ src/components/ScoreForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal } from 'react-materialize';
+import { Button, Modal, Input, Row } from 'react-materialize';
 import Calculator from './Calculator';
 const ScoreForm = () => {
 	const exercises = ['Push Ups', 'Pull Ups', 'Running', 'Cycling']
@@ -12,6 +12,13 @@ const ScoreForm = () => {
 							header={exercise}
 							trigger={<Button>{exercise}</Button>}>
 							<p>Calculator goes here for scoring</p>
+							<Row>
+								<Input placeholder="Placeholder" s={6} label="First Name" />
+								<Input s={6} label="Last Name" />
+								<Input s={12} label="disabled" defaultValue="I am not editable" disabled />
+								<Input type="password" label="password" s={12} />
+								<Input type="email" label="Email" s={12} />
+							</Row>
 						</Modal>
 						<p>{exercise}: ###</p>	
 					</div>

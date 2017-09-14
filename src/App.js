e4747dbab73f86ea892ada1399c21f4cab00da30
DarrenZLew <dlew1990@gmail.com>
2017-09-13 19:51:35 -0500

    Update ScoreForm new buttons layout

--- src/App.js
+++ src/App.js
@@ -4,13 +4,50 @@ import HeaderNavbar from './components/HeaderNavbar';
 import ScoreForm from './components/ScoreForm';
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			exercises: []
+		}
+	}
+
+	componentDidMount() {
+	// Exercise array with inital value of each exercise
+	const exercises = [
+		{	name: 'Push Ups',
+			value: 20,
+			type: 'interval' 
+		},
+		{	name: 'Pull Ups',
+			value: 0, 
+			type: 'interval' 
+		},
+		{	name: 'Running',
+			value: 100, 
+			type: 'timer' 
+		},
+		{	name: 'Cycling',
+			value: 0,
+			type: 'timer'  
+		}		 
+	]		
+
+	this.setState({exercises})
+	}
+
+	// Update value for selected exercise 
+	handleExerciseInputChange = (event, index) => {
+		let exercises = this.state.exercises.slice()
+		exercises[index].value = event.target.value
+		this.setState({exercises})
+	}
+
   render() {
     return (
       <div>
         <HeaderNavbar />
-        <ScoreForm />
+        <ScoreForm exercises={this.state.exercises}/>
       </div>
-
 
     )
   }
--- src/components/ScoreForm.js
+++ src/components/ScoreForm.js
@@ -1,65 +1,68 @@
-import React, { Component } from 'react';
-import { Button, Modal, Input, Row } from 'react-materialize';
+import React from 'react';
+import { Button, Row, Col } from 'react-materialize';
+import '../styles/ScoreForm.css';
 
-// Exercise array with inital value of each exercise
-const exercises = [
-	{	name: 'Push Ups',
-		value: '0' 
-	},
-	{	name: 'Pull Ups',
-		value: 0 
-	},
-	{	name: 'Running',
-		value: 0 
-	},
-	{	name: 'Cycling',
-		value: 0 
-	}		 
-]		
+const ScoreForm = ({ exercises }) => {
+	return (
+		<form className='container center'>
+			{exercises.map((exercise, index) => {
+				const value = exercises[index].value.toString()
+				return (
+					<div key={exercise.name}>
+						<h3>{exercise.name}</h3>
+						{exercise.type === 'interval' && <IntegerButton value={value} />}
+						{exercise.type === 'timer' && <TimerButton value={value}/>}			
+					</div>
+				)
+			})}	
+			<Button className='exercise-save'>Save</Button>
+		</form>
+	)
+}
 
-class ScoreForm extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			exercises: exercises 
-		}
-	}
+const IntegerButton = ({ value }) => (
+	<div>
+		<Row>
+			<Button className='red exercise-minus'>--</Button>
+			<Button className='red exercise-minus'>-</Button>
+			<Button className='exercise-input'>{value}</Button>
+			<Button className='green exercise-plus'>+</Button>
+			<Button className='green exercise-plus'>++</Button>
+		</Row>
+		<Row className='exercise-current-value'>Current: {value}</Row>
+	</div>
+)
 
-	// Update value for selected exercise 
-	handleExerciseInputChange = (event, index) => {
-		let exercises = this.state.exercises.slice()
-		exercises[index].value = event.target.value
-		this.setState({exercises})
-	}
+const TimerButton = ({ value }) => {
+	let hrs = Math.floor(value / 60)
+	let mins = value % 60
 
-	render() {
-		return (
-			<form className='container center'>
-				{exercises.map((exercise, index) => {
-					const errorMessage = 'Enter a number between 0 - 9999'
-					const value = this.state.exercises[index].value.toString()
-					return (
-						<div key={exercise.name}>
-							<Modal
-								header={exercise.name}
-								trigger={<Button>{exercise.name}</Button>}>
-								<Input 
-									type='number' 
-									name={exercise.name} 
-									validate 
-									min='0'
-									max='9999'
-									error={errorMessage}
-									defaultValue={value}
-									onChange={(event) => this.handleExerciseInputChange(event, index)} />
-							</Modal>
-							<Row>{exercise.name}: {value}</Row>
-						</div>
-					)
-				})}	
-			</form>
-		)
-	}
+	return(
+		<div>
+			<Row>
+				<div className='exercise-timer-row'>
+					<span>Hrs</span>
+					<Button className='red exercise-hour'>-1</Button>
+					<Button className='green exercise-hour'>+1</Button>
+				</div>
+			</Row>
+			<Row>
+				<div className='exercise-timer-row'>
+					Mins
+					<Button className='red exercise-fifteen'>-15</Button>
+					<Button className='red exercise-five'>-5</Button>
+					<Button className='green exercise-five'>+5</Button>
+					<Button className='green exercise-fifteen'>+15</Button>
+				</div>
+			</Row>
+			<Row>
+				<span>Total Time </span>
+				<span>Hrs</span><Button>{hrs}</Button>
+				<span>Mins</span><Button>{mins}</Button>
+			</Row>
+			<Row>Current Total Time: {hrs} : {mins}</Row>
+		</div>
+	)
 }
 
 export default ScoreForm

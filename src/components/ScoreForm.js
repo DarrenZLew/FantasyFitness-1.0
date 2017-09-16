5b02516b7a141c88c2db1621cf3bdcf4bbdb7fad
DarrenZLew <dlew1990@gmail.com>
2017-09-15 19:03:54 -0500

    Update score form to a table form

--- src/components/ScoreForm.js
+++ src/components/ScoreForm.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import { Button, Row, Col } from 'react-materialize';
+import { Table, Button, Row, Col } from 'react-materialize';
 import NumericInput from 'react-numeric-input';
 import '../styles/ScoreForm.css';
 
 const ScoreForm = ({ exercises }) => {
 	return (
 		<form className='container center scoreForm-form'>
-			<Row>
-				<Col s={2} offset='s3'><h5>Activity</h5></Col>
-				<Col s={2}><h5>Current Value</h5></Col>
-				<Col s={2}><h5>New Value</h5></Col>
-			</Row>
-			{exercises.map((exercise, index) => {
-				return (
-					<Row key={exercise.name}>
-						<Col s={2} offset='s3'>{exercise.name}</Col>
-						{exercise.type === 'interval' && <IntegerExercise value={exercise.value}/>}
-						{exercise.type === 'timer' && <TimerExercise value={exercise.value}/>}
-					</Row>			
-				)
-			})}	
+			<Table>
+				<thead>
+					<tr>
+						<th><h5>Activity</h5></th>
+						<th><h5>Current Value</h5></th>
+						<th><h5>New Value</h5></th>
+					</tr>
+				</thead>
+				<tbody>
+					{exercises.map((exercise, index) => {
+						return (
+							<tr key={exercise.name}>
+								<td>{exercise.name}</td>
+								{exercise.type === 'interval' && <IntegerExercise value={exercise.value}/>}
+								{exercise.type === 'timer' && <TimerExercise value={exercise.value}/>}
+							</tr>			
+						)
+					})}	
+				</tbody>
+			</Table>
 			<Button className='exercise-save'>Save</Button>
 		</form>
 	)
 }
 
 const IntegerExercise = ({ value }) => (
-	<div>
-		<Col s={2}>{value}</Col>
-		<Col s={2} className='exercise-col-integer-input'><NumericInput style={false} className='exercise-integer-input' min={0} max={9999} defaultValue={0} /></Col>	
-	</div>
+	<td>{value}</td>
+	<td className='exercise-col-integer-input'><NumericInput style={false} className='exercise-integer-input' min={0} max={9999} defaultValue={0} /></td>	
 )
 
 const TimerExercise = ({ value }) => {
@@ -39,8 +43,8 @@ const TimerExercise = ({ value }) => {
 
 	return (
 		<div>
-			<Col s={2}>{currTime}</Col>
-			<Col s={2}>Hrs: {h}  Mins: {m}</Col>
+			<td>{currTime}</td>
+			<td>Hrs: {h}  Mins: {m}</td>
 		</div>
 	)
 }

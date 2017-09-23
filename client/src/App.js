303b3a8443963511b8601c8030f5780477642a24
DarrenZLew <dlew1990@gmail.com>
2017-09-23 00:18:21 -0500

    Update ScoreForm with redux using redux-form

--- client/src/App.js
+++ client/src/App.js
@@ -8,117 +8,14 @@ import Login from './components/Login';
 import FourOFour from './components/404';
 
 class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			exercises: [],
-			bonuses: []
-		}
-		this.handleCheckedBox = this.handleCheckedBox.bind(this)
-
-	}
-
-	componentDidMount() {
-		// Exercise array with inital value of each exercise
-		const exercises = [
-			{	name: 'Push Ups',
-				value: 20,
-				type: 'interval',
-				double: [1]
-			},
-			{	name: 'Pull Ups',
-				value: 0, 
-				type: 'interval' 
-			},
-			{	name: 'Air Squats',
-				value: 0, 
-				type: 'interval' 
-			},
-			{	name: 'Distance Swam',
-				value: 1000,
-				units: 'Miles',
-				type: 'interval'  
-			},			
-			{	name: 'Distance Ran',
-				value: 100,
-				units: 'Miles', 
-				type: 'interval' 
-			},
-			{	name: 'Distance Cycled',
-				value: 370,
-				units: 'Miles',
-				type: 'interval'  
-			},
-			{	name: 'Distance Rowed',
-				value: 20,
-				units: 'Meters',
-				type: 'interval'  
-			},								
-			{	name: '1-Minute Planks',
-				value: 37,
-				type: 'interval'  
-			},				
-			{	name: 'Low Intensity',
-				value: 370,
-				type: 'timer'  
-			},				
-			{	name: 'High Intensity',
-				value: 200,
-				type: 'timer'  
-			},				
-			{	name: 'Sports',
-				value: 370,
-				type: 'timer'  
-			}
-		]
-
-		const bonuses = [
-			{	name: 'Nutrition',
-				mo: false,
-				tu: true,
-				we: true,
-				th: false,
-				fr: false,
-				sa: false,
-				su: false
-			},
-			{	name: 'Stretching',
-				mo: true,
-				tu: true,
-				we: false,
-				th: false,
-				fr: true,
-				sa: false,
-				su: false
-			},
-			{	name: 'Tabata',
-				mo: false,
-				tu: true,
-				we: true,
-				th: false,
-				fr: true,
-				sa: false,
-				su: false
-			}			
-		]		
-
-		this.setState({
-			exercises: exercises,
-			bonuses: bonuses
-		})
-	}
-
-	handleCheckedBox = event => {
-		console.log(event.target)
-	}
-
+  
   render() {
     return (
       <Router>
 				<div>
         	<MenuHeader />
 					<Switch>
-						<Route path='/score' render={() => <ScoreForm exercises={this.state.exercises} bonuses={this.state.bonuses} handleCheckedBox={this.handleCheckedBox}/>} />
+						<Route path='/score' render={() => <ScoreForm />} />
 						<Route path='/login' render={() => <Login />} />
 						<Route path='/404' component={FourOFour} />
 						<Redirect to='/404' />
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'
 import auth from './auth';
+import scoreForm from './scoreForm';
 
 export default combineReducers({
   auth,
+  scoreForm,
+  form: formReducer
 });
 

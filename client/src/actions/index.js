0c7d81094599ad2bd9f02b7d0c4e5122344c56b1
rruenroeng <rruenroeng@gmail.com>
2017-11-05 17:38:22 -0600

    Add small changes to fields. Also add back ProfileForm actions to index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import * as Auth from './auth';
 import * as ScoreFormActions from './scoreForm';
 import * as ScoreSheetActions from './scoreSheet';
+import * as ProfileFormActions from './profileForm';
 
-export { Auth, ScoreFormActions, ScoreSheetActions };
+export { Auth, ScoreFormActions, ScoreSheetActions,ProfileFormActions };

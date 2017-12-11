e7c2a239eb89bf554c6dd78a13b422353d4f0099
Alex Sherman <asherman1024@gmail.com>
2017-12-10 21:12:44 -0600

    Missed file

---
+++ server/queries/db_schema.js
@@ -0,0 +1,47 @@
+
+const sql = require('sql');
+
+exports.users = sql.define({
+    name: 'users',
+    columns: [
+        'id',
+        'name',
+        'email',
+        'hash',
+        'salt',
+        'bio'
+    ]
+});
+
+exports.activities = sql.define({
+    name: 'activities',
+    columns: [
+        'id',
+        'name',
+        'points',
+        'type',
+        'units',
+        'source'
+    ]
+});
+
+exports.uad = sql.define({
+    name: 'user_activity_day',
+    columns: [
+        'id',
+        'user',
+        'activity',
+        'day',
+        'amount',
+        'active'
+    ]
+})
+
+exports.activity_league = sql.define({
+    name: 'activity_league',
+    columns: [
+        'id',
+        'activity',
+        'league'
+    ]
+})
